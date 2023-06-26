7e1cabf919f556505d43e4325405dc37231b5643
HEMANTHKUMARMEDIPALLI <126873530+HEMANTHKUMARMEDIPALLI@users.noreply.github.com>
2023-06-26 19:29:46 +0530

    Add files via upload

---
+++ Band Name Generator.py
@@ -0,0 +1,9 @@
+# Create a Greeting for your Program.
+print(" Hi, Welcome to  the Band Name Generator")
+# Ask for their city
+city = input("what is your city?\n" )
+# Ask for the name of their pet
+pet = input("What is the Name of your Pet?\n")
+# Combine city and pet name along with your band name could be
+print("Your Band Name Could be " + city +" " + pet)     
+# Make sure the input cursor shows on a new line
